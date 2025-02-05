176c319749387bd0e2e8a54c8f111120128f69a6
Rafael <rafaelluna10@hotmail.com>
2025-02-05 16:24:05 -0300

    private

--- src/main/java/br/com/rafaelluna/todolist/user/UserController.java
+++ src/main/java/br/com/rafaelluna/todolist/user/UserController.java
@@ -1,5 +1,30 @@
 package br.com.rafaelluna.todolist.user;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/users")
+
+//Public
+//Private
+//Protected
+
 public class UserController {
     
+    //String - texto
+    //Integer - inteiros
+    //Double - decimais
+    //Float - 
+    //char - caracter
+    //date
+    //void
+    @PostMapping("/")
+    
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
 }
