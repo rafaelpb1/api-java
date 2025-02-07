bb81becbcfc8e3d3cfd0e7c628db1a18ae53cc12
Rafael <rafaelluna10@hotmail.com>
2025-02-07 16:41:12 -0300

    tabela

--- src/main/java/br/com/rafaelluna/todolist/user/UserController.java
+++ src/main/java/br/com/rafaelluna/todolist/user/UserController.java
@@ -3,11 +3,25 @@ package br.com.rafaelluna.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
 @RequestMapping("/users")
+
+public class UserController {
+    
+    @Autowired
+    private IUserRepository userRepository;
+    
+    @PostMapping("/")
+    
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated  = this.userRepository.save(userModel);
+        return userCreated;
+    }
+}
 
 //Public
 //Private
@@ -16,18 +30,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 //      Getters     and     Setters 
 //  Buscar valor    //      Inserir valor
 
-public class UserController {
-    
-    //String - texto
+//String - texto
     //Integer - inteiros
     //Double - decimais
     //Float - 
     //char - caracter
     //date
     //void
-    @PostMapping("/")
-    
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
-    }
-}
